236c3e248eebad7a0580219028db487042585335
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-20 02:59:19 +0900

    Add helper

---
+++ lib/yomou/helper.rb
@@ -0,0 +1,39 @@
+module Yomou
+  module Helper
+
+    def quarter_periods
+      d = Date.new(2013, 5, 1)
+      periods = []
+      while d < Date.today
+        periods << d
+        d = d.next_month
+      end
+      periods
+    end
+
+    def monthly_periods
+      quarter_periods
+    end
+
+    def weekly_periods
+      d = Date.new(2013, 5, 7)
+      periods = []
+      while d < Date.today
+        periods << d
+        d = d.next_day(7)
+      end
+      periods
+    end
+
+    def daily_periods
+      d = Date.new(2013, 5, 7)
+      periods = []
+      while d < Date.today
+        periods << d
+        d = d.next_day
+      end
+      periods
+    end
+
+  end
+end
