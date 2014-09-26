e27385e632ec86a43e1bb92763205aae640d678d
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-27 04:31:19 +0900

    Fix a typo

--- lib/yomou/const.rb
+++ lib/yomou/const.rb
@@ -3,7 +3,7 @@ module Yomou
   YOMOU_DEFAULT_CONFIG = {
     "gzip" => 5,
     "out" => "yaml",
-    "narou_novel" = > "小説データ/小説家になろう",
+    "narou_novel" => "小説データ/小説家になろう",
     "database" => "~/.yomou/db/yomou.db"
   }
 
