ac632223785191fe793ac41196880ab3e86b4ad9
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-27 13:44:16 +0900

    Get downloaded flag from database

--- lib/yomou/downloader.rb
+++ lib/yomou/downloader.rb
@@ -4,11 +4,10 @@ module Yomou
     class Downloader
 
       def initialize
+        @conf = Yomou::Config.new
       end
 
       def glob_downloaded_ncodes
-        @conf = Yomou::Config.new
-
         downloaded = []
         Dir.chdir(@conf.directory) do
           Dir.glob("#{@conf.narou_novel}/n*/") do |dir|
@@ -19,12 +18,30 @@ module Yomou
         downloaded
       end
 
+      def downloaded_ncodes
+        Groonga::Context.default_options = {:encoding => :utf8}
+        return [] unless File.exist?(@conf.database)
+        Groonga::Database.open(@conf.database)
+
+        novels = Groonga["NarouNovels"]
+        p novels.size
+        ncodes = []
+        records = novels.select do |record|
+          record.yomou_status == YOMOU_NOVEL_DOWNLOADED
+        end
+        records.each do |record|
+          ncodes << record._key
+        end
+        ncodes
+      end
+
       def download(ncodes)
         succeeded = []
         failed = []
+        downloaded = downloaded_ncodes
         Dir.chdir(@conf.directory) do
           ncodes.each do |ncode|
-            if @downloaded.include?(ncode)
+            if downloaded.include?(ncode)
               puts "Already downloaded #{ncode}"
             else
               system("narou download --no-convert #{ncode}")
