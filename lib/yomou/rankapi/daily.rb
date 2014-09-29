af1e09d5017a3722e696d14650ca1c100b6a90f9
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-29 22:46:36 +0900

    Rewrite daily list,download command

--- lib/yomou/rankapi/daily.rb
+++ lib/yomou/rankapi/daily.rb
@@ -3,25 +3,35 @@ require "open-uri"
 module Yomou
   module Rankapi
 
-    class Rank < Thor
+    class Daily < Thor
 
       include Yomou::Helper
 
-      private
+      desc "list", ""
+      def list
+        @conf = Yomou::Config.new
+        date = Date.today
+        url = daily_url(date)
+        yaml_gz(url).each do |entry|
+          printf("%3d: %3dpt %s\n",
+                 entry["rank"], entry["pt"], entry["ncode"])
+        end
+      end
 
-      def daily
-        date = Date.new(2013, 5, 7)
+      desc "download [OPTIONS]", ""
+      option :since
+      def download
+        @conf = Yomou::Config.new
+        date = nil
+        if options.has_key?("since")
+          date = Date.parse(options["since"])
+        else
+          date = Date.new(2013, 5, 7)
+        end
         while date < Date.today
-          rtype = date.strftime("%Y%m%d-d")
-          path = Pathname.new(File.join(@conf.directory, "rankapi",
-                                        date.strftime("daily/%Y/%m"),
-                                        "#{rtype}.yaml.gz"))
-          url = [
-            "#{BASE_URL}/?rtype=#{rtype}",
-            "gzip=#{@conf.gzip}",
-            "out=#{@conf.out}"
-          ].join("&")
-          if File.exists?(path.to_s)
+          url = daily_url(date)
+          path = daily_path(date)
+          if path.exist?
             date = date.next_day
             next
           end
@@ -33,6 +43,25 @@ module Yomou
         end
       end
 
+      private
+
+      def daily_url(date)
+        rtype = date.strftime("%Y%m%d-d")
+        [
+          "#{BASE_URL}/?rtype=#{rtype}",
+          "gzip=#{@conf.gzip}",
+          "out=#{@conf.out}"
+        ].join("&")
+      end
+
+      def daily_path(date)
+        rtype = date.strftime("%Y%m%d-d")
+        path = Pathname.new(File.join(@conf.directory,
+                                      "rankapi",
+                                      date.strftime("daily/%Y/%m"),
+                                      "#{rtype}.yaml.gz"))
+        path
+      end
     end
   end
 end
