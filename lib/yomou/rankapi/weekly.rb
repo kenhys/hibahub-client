e1547e7cc87fe3a323ea79b2e016ce4031467772
HAYASHI Kentaro <kenhys@gmail.com>
2014-09-29 23:08:19 +0900

    Rewrite weekly list,download command

--- lib/yomou/rankapi/weekly.rb
+++ lib/yomou/rankapi/weekly.rb
@@ -3,33 +3,65 @@ require "open-uri"
 module Yomou
   module Rankapi
 
-    class Rank < Thor
+    class Weekly < Thor
+
+      include Yomou::Helper
+
+      desc "list", ""
+      def list
+        @conf = Yomou::Config.new
+        date = Date.today
+        until date.tuesday?
+          date = date.prev_day(1)
+        end
+        url = weekly_url(date)
+        yaml_gz(url).each do |entry|
+          printf("%3d: %3dpt %s\n",
+                 entry["rank"], entry["pt"], entry["ncode"])
+        end
+      end
+
+      desc "download [OPTIONS]", ""
+      option :since
+      def download
+        @conf = Yomou::Config.new
+        date = nil
+        if options.has_key?("since")
+          date = Date.parse(options["since"])
+          until date.tuesday?
+            date = date.next_day(1)
+          end
+        else
+          date = Date.new(2013, 5, 7)
+        end
+        while date < Date.today
+          rtype = date.strftime("%Y%m%d-w")
+          url = weekly_url(date)
+          path = weekly_path(date)
+          p url
+          p path
+          save_as(url, path)
+          date = date.next_day(7)
+        end
+      end
 
       private
 
-      def weekly
-        date = Date.new(2013, 5, 7)
-        while date < Date.today
-          rtype = date.strftime("%Y%m%d-w")
-          path = Pathname.new(File.join(@conf.directory, "rankapi",
-                                        date.strftime("weekly/%Y/#{rtype}.yaml.gz")))
-          url = [
-            "#{BASE_URL}/?rtype=#{rtype}",
-            "gzip=#{@conf.gzip}",
-            "out=#{@conf.out}"
-          ].join("&")
-          FileUtils.mkdir_p(path.dirname)
-          if date.tuesday?
-            p url
-            p path.to_s
-            open(url) do |context|
-              File.open(path.to_s, "w+") do |file|
-                file.puts(context.read)
-              end
-            end
-          end
-          date = date.next_day(7)
-        end
+      def weekly_url(date)
+        rtype = date.strftime("%Y%m%d-w")
+        [
+          "#{BASE_URL}/?rtype=#{rtype}",
+          "gzip=#{@conf.gzip}",
+          "out=#{@conf.out}"
+        ].join("&")
+      end
+
+      def weekly_path(date)
+        rtype = date.strftime("%Y%m%d-w")
+        path = Pathname.new(File.join(@conf.directory, "rankapi",
+                                      date.strftime("weekly/%Y/#{rtype}.yaml.gz")))
+        FileUtils.mkdir_p(path.dirname)
+        path
       end
 
     end
