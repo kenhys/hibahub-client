0fd0060ec643422e024087bc49dbc2ffd8a97cbb
HAYASHI Kentaro <kenhys@gmail.com>
2014-10-01 00:48:42 +0900

    Remove unused code

--- lib/yomou/secondrank.rb
+++ lib/yomou/secondrank.rb
@@ -27,54 +27,6 @@ module Yomou
       desc "yearly [OPTIONS]", ""
       subcommand "yearly", Yomou::SecondRankapi::Yearly
 
-      desc "get", "get novel which is ranked on secondlist"
-      option :type
-      def get
-        @conf = Yomou::Config.new
-        return unless options['type']
-
-        types = ["daily", "weekly", "monthly", "quarter", "yearly"]
-        return unless types.include?(options['type'])
-
-        query = options['type'] + "_total"
-        url = sprintf("%s/%s", BASE_URL, query)
-        path = Pathname.new(File.join(@conf.directory,
-                                      "rankapi",
-                                      "secondlist/#{query}.html"))
-        p url
-        download_html(url, path)
-
-        p path
-        entries = extract_rank_h(path)
-        pp entries
-
-        ncodes = entries.collect do |entry|
-          entry["ncode"]
-        end
-        p ncodes
-        Yomou::Narou::Downloader.new.download(ncodes)
-      end
-
-      private
-
-      def download_html(url, path)
-        p url
-        p path
-        if File.exists?(path.to_s)
-          if path.mtime > Time.now - 60 * 60 * 24
-            return
-          end
-        end
-        FileUtils.mkdir_p(path.dirname)
-        open(url) do |context|
-          p path
-          File.open(path.to_s, "w+") do |file|
-            file.puts(context.read)
-          end
-        end
-      end
-
-
     end
   end
 end
