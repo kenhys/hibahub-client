f4dc52fd842095b9dc567a05d6adb9c4790d11c4
HAYASHI Kentaro <kenhys@gmail.com>
2015-10-14 00:04:06 +0900

    Extract as #parse

--- lib/yomou/command/impression.rb
+++ lib/yomou/command/impression.rb
@@ -120,43 +120,7 @@ module Yomou
         rescue
           return hash
         end
-        html_xz(path.to_s) do |doc|
-          doc.xpath("//ul[@id='head_nav']/li/a").each do |a|
-            case a.text
-            when '感想'
-              hash[:impression_url] = a.attribute('href').value
-              hash[:impression_url] =~ /.+\/(\d+)\/$/
-              hash[:impression_id] = $1
-            when 'レビュー'
-              hash[:review_url] = a.attribute('href').value
-            end
-          end
-          doc.xpath("//table[@id='noveltable2']/tr").each_with_index do |tr,i|
-            label = ""
-            text = ""
-            tr.xpath('th').each do |th|
-              label = th.text
-            end
-            tr.xpath('td').each do |td|
-              text = td.text
-            end
-            case label
-            when '感想'
-              hash[:impression_count] = text.gsub(/\n|件/, "").to_i
-            when 'レビュー'
-              hash[:review_count] = text.gsub(/,|件/, "").to_i
-            when 'ポイント評価'
-              unless text.end_with?("非公開")
-                hash[:writing_point] = text.split[0].gsub(/,|pt/, "").to_i
-                hash[:story_point] = text.split[2].gsub(/,|pt/, "").to_i
-              end
-            when 'ブックマーク登録'
-              unless text.end_with?("非公開")
-                hash[:bookmark_count] = text.gsub(/,|件/, "").to_i
-              end
-            end
-          end
-        end
+        hash = @info.parse
         hash
       end
     end
