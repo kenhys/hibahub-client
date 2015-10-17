ede7e2b3a43b60826b638c58ba65faa20cd6ff96
HAYASHI Kentaro <kenhys@gmail.com>
2015-10-17 14:32:37 +0900

    Add #url test case

--- test/novelinfo.rb
+++ test/novelinfo.rb
@@ -24,4 +24,19 @@ class NovelInfoTest < Test::Unit::TestCase
     end
   end
 
+  sub_test_case "url" do
+    test "valid" do
+      ncode = "x12345"
+      info = Yomou::NovelInfo::PageParser.new(ncode)
+      expected = "http://ncode.syosetu.com/novelview/infotop/ncode/x12345/"
+      assert_equal(expected, info.url)
+    end
+
+    test "downcase ncode" do
+      ncode = "X12345"
+      info = Yomou::NovelInfo::PageParser.new(ncode)
+      expected = "http://ncode.syosetu.com/novelview/infotop/ncode/x12345/"
+      assert_equal(expected, info.url)
+    end
+  end
 end
