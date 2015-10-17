8fb461cd1ec5945210a71ee68ae9f4d5e4579f5c
HAYASHI Kentaro <kenhys@gmail.com>
2015-10-17 14:41:21 +0900

    Use data driven test case

--- test/novelinfo.rb
+++ test/novelinfo.rb
@@ -9,33 +9,27 @@ class NovelInfoTest < Test::Unit::TestCase
   end
 
   sub_test_case "cache_path" do
-    test "valid" do
-      ncode = "x12345"
+    expected = "/tmp/info/12/x12345.html.xz"
+    data(
+      'valid' => ["x12345", expected],
+      'unified to downcase?' => ["X12345", expected]
+    )
+    def test_cache_path?(data)
+      ncode, expected = data
       info = Yomou::NovelInfo::PageParser.new(ncode)
-      expected = "/tmp/info/12/x12345.html.xz"
-      assert_equal(expected, info.cache_path.to_s)
-    end
-
-    test "downcase ncode" do
-      ncode = "X12345"
-      info = Yomou::NovelInfo::PageParser.new(ncode)
-      expected = "/tmp/info/12/x12345.html.xz"
       assert_equal(expected, info.cache_path.to_s)
     end
   end
 
   sub_test_case "url" do
-    test "valid" do
-      ncode = "x12345"
+    expected = "http://ncode.syosetu.com/novelview/infotop/ncode/x12345/"
+    data(
+      'valid' => ["x12345", expected],
+      'unified to downcase?' => ["X12345", expected]
+    )
+    def test_url?(data)
+      ncode, expected = data
       info = Yomou::NovelInfo::PageParser.new(ncode)
-      expected = "http://ncode.syosetu.com/novelview/infotop/ncode/x12345/"
-      assert_equal(expected, info.url)
-    end
-
-    test "downcase ncode" do
-      ncode = "X12345"
-      info = Yomou::NovelInfo::PageParser.new(ncode)
-      expected = "http://ncode.syosetu.com/novelview/infotop/ncode/x12345/"
       assert_equal(expected, info.url)
     end
   end
