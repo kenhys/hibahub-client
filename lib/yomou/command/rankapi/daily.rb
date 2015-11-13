06dcdcac5145f2dfe4b80243f515913fb4830561
HAYASHI Kentaro <kenhys@gmail.com>
2015-11-14 02:19:33 +0900

    Use DailyDownloader

--- lib/yomou/command/rankapi/daily.rb
+++ lib/yomou/command/rankapi/daily.rb
@@ -10,7 +10,6 @@ module Yomou
 
       desc "list", ""
       def list
-        @conf = Yomou::Config.new
         date = Date.today
         url = daily_url(date)
         yaml_gz(url).each do |entry|
@@ -23,45 +22,15 @@ module Yomou
       option :since
       def download
         @conf = Yomou::Config.new
-        date = nil
+        @agent = Yomou::Api::RankGet::DailyDownloader.new
         if options.has_key?("since")
-          date = Date.parse(options["since"])
-        else
-          date = Date.new(2013, 5, 1)
+          @agent.since = Date.parse(options["since"])
         end
-        while date < Date.today
-          url = daily_url(date)
-          path = daily_path(date)
-          unless path.exist?
-            if date >= Date.new(2013, 5, 1)
-              p url
-              p path
-              archive(yaml_gz(url), path)
-            end
-          end
-          date = date.next_day
-        end
+        @agent.downloads
       end
 
       private
 
-      def daily_url(date)
-        rtype = date.strftime("%Y%m%d-d")
-        [
-          "#{BASE_URL}/?rtype=#{rtype}",
-          "gzip=#{@conf.gzip}",
-          "out=#{@conf.out}"
-        ].join("&")
-      end
-
-      def daily_path(date)
-        rtype = date.strftime("%Y%m%d-d")
-        path = Pathname.new(File.join(@conf.directory,
-                                      "rankapi",
-                                      date.strftime("daily/%Y/%m"),
-                                      "#{rtype}.yaml.xz"))
-        path
-      end
     end
   end
 end
