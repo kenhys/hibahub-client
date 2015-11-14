42b004a82bccfe69967c307a2696ba6dd3f713c6
HAYASHI Kentaro <kenhys@gmail.com>
2015-11-14 20:24:45 +0900

    Add KeywordSearcher

--- lib/yomou/keyword.rb
+++ lib/yomou/keyword.rb
@@ -35,6 +35,71 @@ module Yomou
         end
         @keywords = yaml_xz(@keywords_path)
       end
+
+      def novels_with_keyword(keyword)
+        load_keywords
+        unless @keywords.has_key?(keyword)
+          return
+        end
+        index = @keywords[keyword]
+        params = {
+          "keyword" => keyword,
+          "id" => index
+        }
+        @agent = KeywordSearcher.new(params)
+        @agent.last_page = 10
+        @agent.crawl
+      end
+    end
+
+    class KeywordSearcher
+      attr_accessor :keyword
+      attr_accessor :page
+      attr_accessor :last_page
+
+      include Yomou::Helper
+
+      def initialize(params)
+        @conf = Yomou::Config.new
+        @keyword = params["keyword"]
+        @id = params["id"]
+        url = sprintf("%s?word=%s&order=hyoka&p=1",
+                      "http://yomou.syosetu.com/search.php",
+                      URI.escape(keyword))
+        @page = 1
+        open(url) do |context|
+          doc = Nokogiri::HTML.parse(context.read)
+          doc.xpath("//div[@id='main_search']/b").each do |b|
+            total = b.text.delete(',').to_i
+            @last_page = (total - 1) / 20 + 1
+          end
+          p @last_page
+        end
+      end
+
+      def crawl
+        page = @page
+        until page > @last_page
+          url = sprintf("%s?word=%s&order=hyoka&p=#{page}",
+                        "http://yomou.syosetu.com/search.php",
+                        URI.escape(@keyword))
+          path = pathname_expanded([@conf.directory,
+                                    "keyword",
+                                    @id.to_s,
+                                    "#{page}.html.xz"])
+          p url
+          p path
+          save_as(url, path, {:compress => true})
+          page = page + 1
+          sleep YOMOU_REQUEST_INTERVAL_MSEC
+        end
+      end
+
+      def makecache
+        page = 1
+        until page > @last_page
+        end
+      end
     end
   end
 end
