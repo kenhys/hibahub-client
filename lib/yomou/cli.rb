6c2a1e46eb8e17801a32077698935b5a0054d79d
Kentaro Hayashi <kenhys@gmail.com>
2019-04-27 13:13:11 +0900

    Add genrerank list subcommand
    % teletype add genrerank list --args 'period=nil' -t=minitest

--- lib/yomou/cli.rb
+++ lib/yomou/cli.rb
@@ -18,6 +18,9 @@ module Yomou
     end
     map %w(--version -v) => :version
 
+    require_relative 'commands/genrerank'
+    register Yomou::Commands::Genrerank, 'genrerank', 'genrerank [SUBCOMMAND]', 'Command description...'
+
     require_relative 'commands/secondrank'
     register Yomou::Commands::Secondrank, 'secondrank', 'secondrank [SUBCOMMAND]', 'Command description...'
 
---
+++ lib/yomou/commands/genrerank.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'thor'
+
+module Yomou
+  module Commands
+    class Genrerank < Thor
+
+      namespace :genrerank
+
+      desc 'list [PERIOD]', 'Command description...'
+      method_option :help, aliases: '-h', type: :boolean,
+                           desc: 'Display usage information'
+      def list(period=nil)
+        if options[:help]
+          invoke :help, ['list']
+        else
+          require_relative 'genrerank/list'
+          Yomou::Commands::Genrerank::List.new(period, options).execute
+        end
+      end
+    end
+  end
+end
