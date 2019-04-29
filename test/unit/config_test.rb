f19f7930bffe3d5984a84cc1468635ce35dc6b0f
Kentaro Hayashi <kenhys@gmail.com>
2019-04-29 17:32:34 +0900

    Add config test case

---
+++ test/unit/config_test.rb
@@ -0,0 +1,27 @@
+require 'tmpdir'
+require_relative '../../lib/yomou/config'
+
+class ConfigTest < Test::Unit::TestCase
+  setup do
+  end
+
+  sub_test_case "directory" do
+    def test_directory
+      Dir.mktmpdir do |dir|
+        ENV['YOMOU_HOME'] = File.join(dir, '.yomou')
+        config = Yomou::Config.new
+        expected = ENV['YOMOU_HOME']
+        assert_equal(expected, config.directory)
+      end
+    end
+
+    def test_path
+      Dir.mktmpdir do |dir|
+        ENV['YOMOU_HOME'] = File.join(dir, '.yomou')
+        config = Yomou::Config.new
+        expected = File.join(ENV['YOMOU_HOME'], 'yomou.yaml')
+        assert_equal(expected, config.path)
+      end
+    end
+  end
+end
