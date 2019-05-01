0c1072599bcb16222c6ecd0a748c8c0568af2b68
Kentaro Hayashi <kenhys@gmail.com>
2019-05-01 23:00:28 +0900

    Add atom test case

---
+++ test/unit/atom_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'tmpdir'
+require_relative '../../lib/yomou/atom'
+
+class AtomTest < Test::Unit::TestCase
+  setup do
+  end
+
+  sub_test_case "type" do
+    def test_directory
+      Dir.mktmpdir do |dir|
+        ENV['YOMOU_HOME'] = File.join(dir, '.yomou')
+        output = StringIO.new
+        atom_path = allnovel_path
+        downloader = Yomou::Atom::Downloader.new(output: output)
+        downloader.download
+        assert_equal(true, File.exist?(allnovel_path))
+      end
+    end
+  end
+
+  private
+
+  def allnovel_path
+    relative_path = Time.now.strftime('atomapi/%Y/%m/%d/allnovel-%H%M.Atom.yaml.xz')
+    File.join(ENV['YOMOU_HOME'], relative_path)
+  end
+end
