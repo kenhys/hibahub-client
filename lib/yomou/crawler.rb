5749aea6006377a90b5120b2e2cd720232570d01
Kentaro Hayashi <kenhys@gmail.com>
2019-05-02 14:51:52 +0900

    Simplify nested module

--- lib/yomou/crawler.rb
+++ lib/yomou/crawler.rb
@@ -1,14 +1,13 @@
 module Yomou
-  module Clawer
-    class BaseCrawler
-      def extract_total_novels(doc)
-        total = 0
-        doc.xpath("//div[@class='site_h2']").each do |div|
-          div.text =~ /.+?(\d+)/
-          total = $1.to_i
-        end
-        total
+  class BaseCrawler
+    include Yomou::Helper
+    def extract_total_novels(doc)
+      total = 0
+      doc.xpath("//div[@class='site_h2']").each do |div|
+        div.text =~ /.+?(\d+)/
+        total = $1.to_i
       end
+      total
     end
   end
 end
